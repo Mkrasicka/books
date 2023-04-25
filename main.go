0c1486682548b619bba3d215b4a30cc23327ed58
magdalena krasicka <krasickamagdalena@gmail.com>
2023-04-25 20:47:27 +0100

    get request to display all the books in the home page

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-// import "github.com/gorilla/mux"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 // import (
 //         "encoding/json"
 //         "github.com/gorilla/mux"
@@ -16,9 +24,27 @@ type Books struct {
 }
 
 func main() {
-	var books = []books{
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", handleGetBooks)
+	http.ListenAndServe(":3000", router)
+}
+
+func handleGetBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var books = []Books{
 		{ID: "1", Title: "Educated", Author: "Tara Westover", Quantity: 3},
 		{ID: "2", Title: "Myth of Normal", Author: "Tara Westover", Quantity: 3},
 		{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 3},
 	}
+	booksByte, err := json.Marshal(books)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	// fmt.Println(string(booksByte))
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, string(booksByte))
 }
