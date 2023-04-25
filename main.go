9521c1f9f241f8cbf9baf87d4de866a026cf2042
magdalena krasicka <krasickamagdalena@gmail.com>
2023-04-25 18:53:52 +0100

    created books struct and intialize variable books

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 // import "github.com/gorilla/mux"
 
+type Books struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
 func main() {
+	var books = []books{
+		{ID: "1", Title: "Educated", Author: "Tara Westover", Quantity: 3},
+		{ID: "2", Title: "Myth of Normal", Author: "Tara Westover", Quantity: 3},
+		{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 3},
+	}
 
 }
