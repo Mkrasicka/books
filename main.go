daf83c36eb21ff22e4ca04924addaa1140dff071
magdalena krasicka <krasickamagdalena@gmail.com>
2023-04-26 11:29:07 +0100

    Its not returning appended book from the json file

--- main.go
+++ main.go
@@ -26,24 +26,23 @@ type Book struct {
 	Quantity int    `json:"quantity"`
 }
 
-var books = []Book{}
+var books = []Book{
+	{ID: "1", Title: "Educated", Author: "Tara Westover", Quantity: 3},
+	{ID: "2", Title: "Myth of Normal", Author: "Tara Westover", Quantity: 3},
+	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 3},
+}
 
 func main() {
 	log.Println("Starting server...")
 	router := mux.NewRouter()
 
 	router.HandleFunc("/books", handleGetBooks)
-	router.HandleFunc("/newbook", handleCreateBook)
+	router.HandleFunc("/books", handleCreateBook)
 	http.ListenAndServe(":3000", router)
+
 }
 
 func handleGetBooks(w http.ResponseWriter, r *http.Request) {
-
-	var books = []Book{
-		{ID: "1", Title: "Educated", Author: "Tara Westover", Quantity: 3},
-		{ID: "2", Title: "Myth of Normal", Author: "Tara Westover", Quantity: 3},
-		{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 3},
-	}
 
 	log.Printf("Returning book data: %v\n", books)
 
@@ -68,17 +67,16 @@ func handleCreateBook(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	// Assign a unique ID to the new BOOk
-	newBook.ID = fmt.Sprintf("%d", len(books)+1)
 
 	books = append(books, newBook)
 
 	// return the new book data to the client
-	booksByte, err := json.Marshal(newBook)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	// json.NewEncoder(w).Encode(newBook)
-	io.WriteString(w, string(booksByte))
+	// booksByte, err := json.Marshal(newBook)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+	json.NewEncoder(w).Encode(newBook)
+	fmt.Println(newBook)
+	// io.WriteString(w, string(booksByte))
 }
