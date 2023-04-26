221815a64fede3b46e2ecdac8d000d5caf90fc8b
magdalena krasicka <krasickamagdalena@gmail.com>
2023-04-26 10:44:54 +0100

    Created routed dor adding a new book but book is not saved

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -17,38 +17,69 @@ import (
 //         "net/http"
 // )
 
-	//         "log"
+//         "log"
 
-type Books struct {
+type Book struct {
 	ID       string `json:"id"`
 	Title    string `json:"title"`
 	Author   string `json:"author"`
 	Quantity int    `json:"quantity"`
 }
 
-func main() {
+var books = []Book{}
 
+func main() {
+	log.Println("Starting server...")
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", handleGetBooks)
+	router.HandleFunc("/books", handleGetBooks)
+	router.HandleFunc("/newbook", handleCreateBook)
 	http.ListenAndServe(":3000", router)
 }
 
 func handleGetBooks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 
-	var books = []Books{
+	var books = []Book{
 		{ID: "1", Title: "Educated", Author: "Tara Westover", Quantity: 3},
 		{ID: "2", Title: "Myth of Normal", Author: "Tara Westover", Quantity: 3},
 		{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 3},
 	}
+
+	log.Printf("Returning book data: %v\n", books)
+
+	// fmt.Printf("to jest data type: %T\n", books)
 	booksByte, err := json.Marshal(books)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
 
-	// fmt.Println(string(booksByte))
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, string(booksByte))
+}
 
+func handleCreateBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received POST request for new book")
+	var newBook Book
+
+	err := json.NewDecoder(r.Body).Decode(&newBook)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Assign a unique ID to the new BOOk
+	newBook.ID = fmt.Sprintf("%d", len(books)+1)
+
+	books = append(books, newBook)
+
+	// return the new book data to the client
+	booksByte, err := json.Marshal(newBook)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// json.NewEncoder(w).Encode(newBook)
+	io.WriteString(w, string(booksByte))
 }
